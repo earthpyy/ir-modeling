f9ad6b70f0990d5802b71a660fa81c040e6d0257
Preeti Yuankrathok <preetisatit@gmail.com>
2018-03-28 23:20:51 +0700

    tf, idf, sim (untested)

--- vector/vector.py
+++ vector/vector.py
@@ -1,11 +1,16 @@
 from tf_idf import tf, idf
 
-# class Keyword:
-#     keyword_count = 0
+class Keyword:
+    keyword_count = 0
 
-#     def __init__(self, name):
-#         self.name = name
-#         Keyword.keyword_count += 1
+    def __init__(self, name):
+        self.name = name
+        self.count = 0
+        self.idf = None
+        Keyword.keyword_count += 1
+
+    def __repr__(self):
+        return self.name
 
 class Document:
     document_count = 0
@@ -14,27 +19,46 @@ class Document:
         self.name = name
         self.keywords = {}
         self.tf = {}
-        self.idf = {}
+        self.sim = {}
         Document.document_count += 1
 
-# keywords = []
-# for keyword in input("Keyword (seperate by space): ").split():
-#     keywords.append(Keyword(keyword))
-keywords = input("Keyword (seperate by space): ").split()
+TF_K = 0.5
+
+keywords = []
+for keyword in input("Keyword (seperate by space): ").split():
+    keywords.append(Keyword(keyword))
+# keywords = input("Keyword (seperate by space): ").split()
 
 documents = []
 while (True):
     name = input("Document name/number: ")
     document = Document(name)
 
+    # ask for each keyword count
     for keyword in keywords:
-        document.keywords[keyword] = int(input("[" + name + "] Keyword '" + keyword + "' count: "))
-    
+        document.keywords[keyword] = int(input("[" + name + "] Keyword '" + keyword.name + "' count: "))
+        keyword.count += 1
+
+    # add document to list
     documents.append(document)
 
     prompt = input('Do you want to add more documents? (y/n): ')
     if (prompt not in ['', 'y', 'yes']):
         break
 
-# for document in documents:
-#     print(vars(document))
+# find idf
+for keyword in keywords:
+    keyword.idf = idf(Keyword.keyword_count, keyword.count)
+
+# find tf & sim
+for document in documents:
+    # find maximum frequencies in documents
+    max_freq = document.keywords[max(document.keywords, key=document.keywords.get)]
+
+    # find tf & sim
+    for keyword, freq in document.keywords.items():
+        document.tf[keyword] = tf(freq, max_freq, TF_K)
+        document.sim[keyword] = document.tf[keyword] * keyword.idf
+
+for document in documents:
+    print(vars(document))
