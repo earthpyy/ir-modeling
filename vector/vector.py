79deeaa18fd27527cdc116bc1b670856b812279c
Preeti Yuankrathok <preetisatit@gmail.com>
2018-03-29 00:15:02 +0700

    add deg of sim, abstract class

--- vector/vector.py
+++ vector/vector.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from tf_idf import tf, idf
 
 class Keyword:
@@ -12,17 +13,35 @@ class Keyword:
     def __repr__(self):
         return self.name
 
-class Document:
-    document_count = 0
-
+class WeightableObject(ABC):
     def __init__(self, name):
         self.name = name
         self.keywords = {}
         self.tf = {}
-        self.sim = {}
+        self.weight = {}
+    
+    def __repr__(self):
+        return self.name
+
+class Document(WeightableObject):
+    document_count = 0
+
+    def __init__(self, name):
+        super().__init__(name)
         Document.document_count += 1
 
+class Query(WeightableObject):
+    def __init__(self, name):
+        super().__init__(name)
+        self.deg = {}
+
 TF_K = 0.5
+
+def sum_pow(document):
+    result = 0;
+    for weight in document.weight.values():
+        result += pow(weight, 2)
+    return result
 
 keywords = []
 for keyword in input("Keyword (seperate by space): ").split():
@@ -31,12 +50,11 @@ for keyword in input("Keyword (seperate by space): ").split():
 
 documents = []
 while (True):
-    name = input("Document name/number: ")
-    document = Document(name)
+    document = Document(input("Document name/number: "))
 
     # ask for each keyword count
     for keyword in keywords:
-        document.keywords[keyword] = int(input("[" + name + "] Keyword '" + keyword.name + "' count: "))
+        document.keywords[keyword] = int(input("[" + document.name + "] Keyword '" + keyword.name + "' count: "))
         keyword.count += 1
 
     # add document to list
@@ -50,15 +68,47 @@ while (True):
 for keyword in keywords:
     keyword.idf = idf(Keyword.keyword_count, keyword.count)
 
-# find tf & sim
 for document in documents:
     # find maximum frequencies in documents
     max_freq = document.keywords[max(document.keywords, key=document.keywords.get)]
 
-    # find tf & sim
+    # find tf & weight
     for keyword, freq in document.keywords.items():
         document.tf[keyword] = tf(freq, max_freq, TF_K)
-        document.sim[keyword] = document.tf[keyword] * keyword.idf
+        document.weight[keyword] = document.tf[keyword] * keyword.idf
+
+queries = []
+while (True):
+    query = Query(input("Query name/number: "))
+
+    # ask for each keyword count in query
+    for keyword in keywords:
+        query.keywords[keyword] = int(input("[" + query.name + "] Keyword '" + keyword.name + "' count: "))
+
+    # add query to list
+        queries.append(query)
+
+    prompt = input('Do you want to add more documents? (y/n): ')
+    if (prompt not in ['', 'y', 'yes']):
+        break
+
+for query in queries:
+    max_freq = query.keywords[max(query.keywords, key=query.keywords.get)]
+
+    # find tf & weight
+    for keyword, freq in query.keywords.items():
+        query.tf[keyword] = tf(freq, max_freq, TF_K)
+        query.weight[keyword] = query.tf[keyword] * keyword.idf
+
+    # find degree of similarity
+    for document in documents:
+        query.deg[document] = 0;
+        for keyword, weight in document.weight.items():
+            query.deg[document] += weight * query.weight[keyword]
+        query.deg[document] /= (sum_pow(query) * sum_pow(document))
 
 for document in documents:
     print(vars(document))
+
+for query in queries:
+    print(vars(query))
