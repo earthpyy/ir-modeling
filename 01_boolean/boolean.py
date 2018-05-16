24cf176e1344d3618486ff9dcdff1e51c877968f
Preeti Yuankrathok <preetisatit@gmail.com>
2018-05-16 21:03:01 +0700

    nearly finish boolean

--- 01_boolean/boolean.py
+++ 01_boolean/boolean.py
@@ -7,9 +7,11 @@ with open(os.path.join(sys.path[0], 'sample.csv'), 'r') as fp:
     reader = csv.reader(fp, delimiter=',')
     n = len(next(reader)) - 1
     for i in range(pow(2, n)):
-        keywords.append([])
+        keywords.append(set())
 
     for row in reader:
         documents.append(row[0])
         bin_str = ''.join(row[1:])
-        keywords[int(bin_str, 2)].append(len(documents) - 1)
+        keywords[int(bin_str, 2)].add(len(documents) - 1)
+
+print("Answer:", [documents[i] for i in (keywords[4] | keywords[6] | keywords[7])])
